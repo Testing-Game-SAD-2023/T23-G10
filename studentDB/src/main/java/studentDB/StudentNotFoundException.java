77d3d122aa15c5cc84f22566dc111750879e4e2d
Lucio Ilardi <91966462+Ilardi@users.noreply.github.com>
2023-05-20 15:06:46 +0200

    Cleanup

--- studentDB/src/main/java/studentDB/StudentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package studentDB;
-
-class StudentNotFoundException extends RuntimeException {
-
-	StudentNotFoundException(Long id) {
-		super("Could not find student " + id);
-	}
-}
--- studentDB/src/main/java/studentDB/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package studentDB;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import entity.Student;
-
-interface StudentRepository extends JpaRepository<Student, Long> {
-
-}

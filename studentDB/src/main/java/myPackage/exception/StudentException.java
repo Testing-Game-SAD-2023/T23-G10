eacee818e080ae5bc97ca4a4bf5910ac814b5d84
Lucio Ilardi <91966462+Ilardi@users.noreply.github.com>
2023-05-26 12:19:57 +0200

    Cleanup

--- studentDB/src/main/java/myPackage/exception/StudentException.java
+++
@@ -1,9 +0,0 @@
-package myPackage.exception;
-
-@SuppressWarnings("serial")
-public class StudentException extends RuntimeException {
-
-	public StudentException(String message) {
-		super("Errore: " + message);
-	}
-}

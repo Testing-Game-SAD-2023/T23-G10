50bfa9cb74fee68378f947726dc4dfc45fe194a0
Ilardi <luci.ilardi@studenti.unina.it>
2023-05-29 17:34:44 +0200

    Added https option

---
+++ studentDB/src/main/java/myPackage/service/TLSConfig.java
@@ -0,0 +1,31 @@
+package myPackage.service;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TLSConfig {
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                // Allow both HTTP and HTTPS access
+                context.setUseHttpOnly(false);
+                context.setSessionCookieName("JSESSIONID");
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(createHTTPConnector());
+        return tomcat;
+    }
+
+    private Connector createHTTPConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);
+        return connector;
+    }
+}
